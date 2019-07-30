cea4b7d9c2837947e65cf4c56345398de4d98bff
Paulgbullard <46537148+Paulgbullard@users.noreply.github.com>
2019-07-30 12:22:23 +0100

    Update app.r
    Extended comments on file to aid readability.
    To-do list consolidated.

--- app.R
+++ app.R
@@ -1,39 +1,54 @@
+#Small Shiny app to predict ED demand using FB Prophet (https://facebook.github.io/prophet/)
+#Requires data processed via NHS Demand and Capacity Team ED Model for correct formatting
+#Outputs .CSV file with predicted attendances for the next week (by hour).
+
+#To do:
+#1. Improve UI of app
+#2. Ensure optimisation of code
+#3. Allow user input of some key parameters via a helper header row
+#4. Create 'loading...' notification
+#5. Multiple file upload and processing
+
+#Required libraries:
 library(shiny)
 library(prophet)
 library(anytime)
 library(ggplot2)
 
-# Define UI for data upload app ----
+#Define UI for data upload
 
 ui <- fluidPage(
   
-  titlePanel("NHS Demand and Capacity Implementation of Prophet"), #main page title
+  titlePanel("NHS Demand and Capacity Implementation of Prophet"),
   
-  sidebarLayout( #configure as split page
+  sidebarLayout(
   
-  sidebarPanel(h3("Data Upload"), #sidebar main title
+  #Sidebar panel for user interaction
+  sidebarPanel(h3("Data Upload"),
+               
                fileInput(inputId = 'file1', label = "Upload dataset here:",multiple = TRUE,
                          accept = c("text/csv",
                                     "text/comma-separated-values,text/plain",
                                     ".csv")),
+               
                downloadButton("downloadData", "Download")
-               ),#fileinput widget
+               ),
              
 
     # Main panel for displaying outputs ----
     mainPanel(
       
-      # Output: Plot ----
-      plotOutput("test")
+      plotOutput("forecast")
       
     )
     
   ))
 
 
-# Define server logic to read selected file ----
+#Define server logic to process uploaded file
 server <- function(input, output) {
   
+  #Process data through prophet
   fcst_short <- eventReactive(input$file1, {
     
   df <- read.csv(input$file1$datapath,
@@ -41,11 +56,11 @@ server <- function(input, output) {
                  sep = ","
   )
   
-  df <- tail(df,-8000)
+  df <- tail(df,-8784)
   
   df$ds <- anytime(df$ds)
   
-  m <- prophet()
+  m <- prophet(yearly.seasonality = TRUE)
   m <- add_country_holidays(m,'England')
   m <- fit.prophet(m, df)
   
@@ -54,17 +69,17 @@ server <- function(input, output) {
   
   fcst_short <- tail(fcst, 168)
   
-  
-  
   return(fcst_short)
   })
   
+  #Process outputs to help chart display
   fcst_m <- reactive({
     fcst_m <- max(fcst_short()$yhat_upper)
     return(fcst_m)
   })
   
-  output$test <- renderPlot({
+  #Present outputs in line and area chart
+  output$forecast <- renderPlot({
   
     req(input$file1)
     
@@ -75,6 +90,7 @@ server <- function(input, output) {
 
   })
   
+  #Define download parameters for outputs
   output$downloadData <- downloadHandler(
     filename = function() {
       paste("data-", Sys.Date(), ".csv",sep = "")
